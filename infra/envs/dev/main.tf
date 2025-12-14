cd0416dfce677e593e1f6e2d944f172ff25b1e3a
bhagirath1006 <bhagi.sy99@gmail.com>
2025-12-15 02:25:38 +0530

    dev environment

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -12,6 +12,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Create S3 backend resources (state bucket and locking table)
+module "s3_backend" {
+  source = "../../modules/s3"
+  state_bucket_name      = "bhagirath-eks-terraform-state"
+  state_lock_table_name  = "terraform-state-lock"
+}
+
 module "vpc" {
   source = "../../modules/vpc"
   region = var.region
@@ -23,12 +30,4 @@ module "eks" {
   cluster_name = var.cluster_name
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-}
-
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
 }
